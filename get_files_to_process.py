fabba3373451bc0d0640dcfa6a4876d45ba1ce02
Turtlestoffel <me@turtlestoffel.com>
2023-11-16 21:54:38 +0100

    Consolidate get_file_groups_to_process implementation

--- get_files_to_process.py
+++ get_files_to_process.py
@@ -14,20 +14,17 @@ def get_file_groups_to_process() -> tuple[list[CopyFile], list[MarkdownSourceFil
     # Force the list of files to be sorted to avoid a messy input file
     if not util.is_sorted([file['glob'] for file in config.FILES_TO_PROCESS]):
         raise RuntimeError("List of files to process must be sorted")
+    
+    copy_files = []
+    markdown_files = []
+    for file_group_definition in config.FILES_TO_PROCESS:
+        file_glob = file_group_definition['glob']
+        resolved_file_paths = glob.glob(file_glob, recursive=True)
 
-    copy_file_groups = [_create_copy_files(raw_files) for raw_files in config.FILES_TO_PROCESS if raw_files['type'] == 'copy']
-    copy_files = util.flatten(copy_file_groups)
-    markdown_file_groups = [_create_markdown_files(raw_files) for raw_files in config.FILES_TO_PROCESS if raw_files['type'] == 'text']
-    markdown_files = util.flatten(markdown_file_groups)
+        if file_group_definition['type'] == 'copy':
+            copy_files.extend([CopyFile(file_path, config) for file_path in resolved_file_paths])
+        
+        elif file_group_definition['type'] == 'text':
+            markdown_files.extend([MarkdownSourceFile(file_path, config) for file_path in resolved_file_paths])
     
     return copy_files, markdown_files
-
-def _create_copy_files(input_dict: dict):
-    file_glob = input_dict['glob']
-    resolved_file_paths = glob.glob(file_glob, recursive=True)
-    return [CopyFile(file_path, config) for file_path in resolved_file_paths]
-
-def _create_markdown_files(input_dict: dict):
-    file_glob = input_dict['glob']
-    resolved_file_paths = glob.glob(file_glob, recursive=True)
-    return [MarkdownSourceFile(file_path, config) for file_path in resolved_file_paths]
